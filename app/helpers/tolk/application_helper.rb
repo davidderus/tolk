5db0da2d4fd285f9cab0134a82cc52780fbd7a36
David Dérus <david.derus@kasual.biz>
2015-06-24 12:01:28 +0200

    [*] method_missing is an helper, not a controller method

--- app/helpers/tolk/application_helper.rb
+++ app/helpers/tolk/application_helper.rb
@@ -2,6 +2,14 @@ module Tolk
   module ApplicationHelper
     include Tolk::Pagination::ViewHelper
 
+    def method_missing(method, *args, &block)
+      if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
+        main_app.send(method, *args)
+      else
+        super
+      end
+    end
+
     def format_i18n_value(value)
       value = h(yaml_value(value))
       value = highlight_linebreaks(value)
