c1ae9b88fde0a40d3a9ce140b4dbf3956eb7035c
David Dérus <david.derus@kasual.biz>
2015-06-24 14:04:14 +0200

    [+] Class for form select

--- app/helpers/tolk/application_helper.rb
+++ app/helpers/tolk/application_helper.rb
@@ -51,7 +51,8 @@ module Tolk
 
     def scope_selector_for(locale)
       select_tag 'scope', options_for_select([[Tolk::Locale.primary_locale.language_name, "origin"],
-                                              [locale.language_name, "target"]], params[:scope])
+                                              [locale.language_name, "target"]], params[:scope]),
+                                              :class => 'form-control'
     end
   end
 end
