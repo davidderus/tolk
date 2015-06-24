93cbfc9c59f14f3a9b9754c202c312c3e7262c5f
David Dérus <david.derus@kasual.biz>
2015-06-24 11:53:31 +0200

    [*] Updating engine to be semi-isolated

--- app/controllers/tolk/application_controller.rb
+++ app/controllers/tolk/application_controller.rb
@@ -1,20 +1,18 @@
-module Tolk
-  class ApplicationController < ActionController::Base
-    include Tolk::Pagination::Methods
+class Tolk::ApplicationController < ApplicationController
+  include Tolk::Pagination::Methods
 
-    helper :all
-    protect_from_forgery
+  helper :all
+  protect_from_forgery
 
-    cattr_accessor :authenticator
-    before_filter :authenticate
+  cattr_accessor :authenticator
+  before_filter :authenticate
 
-    def authenticate
+  def authenticate
 #      self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
-      instance_exec(nil, &self.authenticator) if self.authenticator && self.authenticator.respond_to?(:instance_exec)
-    end
+    instance_exec(nil, &self.authenticator) if self.authenticator && self.authenticator.respond_to?(:instance_exec)
+  end
 
-    def ensure_no_primary_locale
-      redirect_to tolk.locales_path if @locale.primary?
-    end
+  def ensure_no_primary_locale
+    redirect_to tolk.locales_path if @locale.primary?
   end
 end
