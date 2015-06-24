47e67a33b85cf350527edc6111d79ce1539f2861
David Dérus <david.derus@kasual.biz>
2015-06-24 16:39:27 +0200

    [+] Better JS here too

--- app/assets/javascripts/tolk/interpolations.js
+++ app/assets/javascripts/tolk/interpolations.js
@@ -1,10 +1,10 @@
 $(function () {
 
   var interpolation = new RegExp("%{\\w+}", "g");
-  
-  $(".translations textarea").bind("change", function () {
-      var row = $(this).parents("tr")
-        , original_text = row.find(".original textarea").val()
+
+  $(".translations textarea, .translations input[type=text]").bind("change", function () {
+      var row = $(this).closest(".row")
+        , original_text = row.find(".original textarea, .original input[type=text]").val()
         , translated_text = $(this).val()
         , original_interpolations = original_text.match(interpolation) || []
         , translated_interpolations = translated_text.match(interpolation) || []
@@ -14,7 +14,7 @@ $(function () {
                   ($(original_interpolations).not(translated_interpolations).length !== 0 ||
                    $(translated_interpolations).not(original_interpolations).length !== 0);
 
-      row.find(".actions .warning").toggle(not_match);
+      row.find(".warning").toggle(not_match);
 
     });
 
--- lib/tolk/version.rb
+++ lib/tolk/version.rb
@@ -1,4 +1,4 @@
 module Tolk
-  VERSION = '1.9.3.21'
+  VERSION = '1.9.3.22'
   # 1.9.3.X = Forked 1.9.3 with revision X
 end
